fc6e9dbc80d4d68d83481d2afa7337dd1ae1cf93
Brett Morris <brettmorris21@gmail.com>
2019-11-07 20:23:56 +0100

    Style tweaks to all files

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 import os
 import logging
 
@@ -8,33 +7,32 @@ __all__ = [
 
 
 def config_logger(log_file="/dev/null", level=logging.INFO):
-    
     class MyFormatter(logging.Formatter):
-        
+
         info_format = "\x1b[32;1m%(asctime)s [%(name)s]\x1b[0m %(message)s"
         error_format = "\x1b[31;1m%(asctime)s [%(name)s] [%(levelname)s]\x1b[0m %(message)s"
-        
+
         def format(self, record):
-            
+
             if record.levelno > logging.INFO:
                 self._style._fmt = self.error_format
             else:
                 self._style._fmt = self.info_format
-            
+
             res = super(MyFormatter, self).format(record)
             return res
-    
+
     rootLogger = logging.getLogger()
-    
+
     fileHandler = logging.FileHandler(log_file)
-    fileFormatter = logging.Formatter("%(asctime)s [%(name)s] [%(levelname)s]> %(message)s")
+    fileFormatter = logging.Formatter(
+        "%(asctime)s [%(name)s] [%(levelname)s]> %(message)s")
     fileHandler.setFormatter(fileFormatter)
     rootLogger.addHandler(fileHandler)
-    
+
     consoleHandler = logging.StreamHandler()
     consoleFormatter = MyFormatter()
     consoleHandler.setFormatter(consoleFormatter)
     rootLogger.addHandler(consoleHandler)
-    
-    rootLogger.setLevel(level)
 
+    rootLogger.setLevel(level)
