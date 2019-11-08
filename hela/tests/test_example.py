652179f1c85babf75bc50e40b903dea7681af1a5
Brett Morris <brettmorris21@gmail.com>
2019-11-08 12:12:18 +0100

    Adding first end-to-end test which checks against linear docs example

---
+++ hela/tests/test_example.py
@@ -0,0 +1,27 @@
+
+
+def test_linear_end_to_end():
+    from ..forest import generate_example_data
+    example_dir, training_dataset, samples_path = generate_example_data()
+
+    # Import RandomForest object from HELA
+    from ..forest import RandomForest
+
+    # Initialize a random forest object:
+    rf = RandomForest(training_dataset, example_dir, samples_path)
+
+    # Train the random forest:
+    r2scores = rf.train(num_trees=1000, num_jobs=1)
+
+    # Do a rough check that the R^2 values are near unity
+    assert abs(r2scores['slope'] - 1) < 0.01
+    assert abs(r2scores['intercept'] - 1) < 0.01
+
+    # Predict posterior distributions from random forest
+    posterior = rf.predict(plot_posterior=False)
+    posterior_slopes, posterior_intercepts = posterior.samples.T
+
+    # Do a very generous check that the posterior distributions match
+    # the expected values
+    assert abs(posterior_slopes.mean() - 0.3) < 0.1
+    assert abs(posterior_intercepts.mean() - 0.5) < 0.1
